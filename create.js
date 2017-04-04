c214da7d5a7bdbb217466c6d3fed59dc3ea59613
Lucinda A Gebbie <lg73551@srcs.loc>
2017-04-04 14:39:27 -0700

    day 1 progress

---
+++ create.js
@@ -0,0 +1,11 @@
+function elementAlert (element) {
+   var aNum = "Atomic Number:";
+   if (element == "Li") {
+       msg  = "LITHIUM:" + '<br>' + aNum + " 3." + '<br>' + "Lithium is the lightest metal!";
+} else if (element == "Be") {
+    msg = "BROMINE";
+ } else if (element ==
+
+    document.getElementById("output").innerHTML = msg;
+}
+
