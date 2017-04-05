f44bcea0d25f7b9100b4cb133e5765d282bf50a2
Chloe Thies <ChloeThies@Chloes-MacBook.local>
2017-04-05 08:47:13 -0700

    small changes

--- create.js
+++ create.js
@@ -1,11 +1,361 @@
-function elementAlert (element) {
-   var aNum = "Atomic Number:";
-   if (element == "Li") {
-       msg  = "LITHIUM:" + '<br>' + aNum + " 3." + '<br>' + "Lithium is the lightest metal!";
-} else if (element == "Be") {
-    msg = "BROMINE";
- } else if (element ==
-
-    document.getElementById("output").innerHTML = msg;
+function elementAlert(element) {
+    //var aNum = "Atomic Number:";
+    if (element == "H") {
+        msg = "HYDROGEN:";
+    }
+    else if (element == "He") {
+        msg = "HELIUM:";
+    }
+    else if (element == "Li") {
+        msg = "LITHIUM:"/* + '<br>' + aNum + " 3." + '<br>' + "Lithium is the lightest metal!"*/;
+    }
+    else if (element == "Be") {
+        msg = "BERYLLIUM:";
+    }
+    else if (element == "B") {
+        msg = "BORON:";
+    }
+    else if (element == "C") {
+        msg = "CARBON:";
+    }
+    else if (element == "N") {
+        msg = "NITROGEN:";
+    }
+    else if (element == "O") {
+        msg = "OXYGEN:";
+    }
+    else if (element == "F") {
+        msg = "FLUORNE:";
+    }
+    else if (element == "Ne") {
+        msg = "NEON:";
+    }
+    else if (element == "Na") {
+        msg = "SODIUM:";
+    }
+    else if (element == "Mg") {
+        msg = "MAGNESIUM:";
+    }
+    else if (element == "Al") {
+        msg = "ALUMINUM:";
+    }
+    else if (element == "Si") {
+        msg = "SILICON:";
+    }
+    else if (element == "P") {
+        msg = "PHOSPHORUS:";
+    }
+    else if (element == "S") {
+        msg = "SULFUR:";
+    }
+    else if (element == "Cl") {
+        msg = "CHLORINE:";
+    }
+    else if (element == "Ar") {
+        msg = "ARGON:";
+    }
+    else if (element == "F") {
+        msg = "FLUORNE:";
+    }
+    else if (element == "K") {
+        msg = "POTASSIUM:";
+    }
+    else if (element == "Ca") {
+        msg = "CALCIUM:";
+    }
+    else if (element == "Sc") {
+        msg = "SCANDIUM:";
+    }
+    else if (element == "Ti") {
+        msg = "TITANIUM:";
+    }
+    else if (element == "V") {
+        msg = "VANADIUM:";
+    }
+    else if (element == "Cr") {
+        msg = "CHROMIUM:";
+    }
+    else if (element == "Mn") {
+        msg = "MANGANESE:";
+    }
+    else if (element == "Fe") {
+        msg = "IRON:";
+    }
+    else if (element == "Co") {
+        msg = "COBALT:";
+    }
+    else if (element == "Ni") {
+        msg = "NICKEL:";
+    }
+    else if (element == "Cu") {
+        msg = "COPPER:";
+    }
+    else if (element == "Zn") {
+        msg = "ZINC:";
+    }
+    else if (element == "Ga") {
+        msg = "GALLIUM:";
+    }
+    else if (element == "Ge") {
+        msg = "GERMANIUM:";
+    }
+    else if (element == "As") {
+        msg = "ARSENIC:";
+    }
+    else if (element == "Se") {
+        msg = "SELENIUM:";
+    }
+    else if (element == "Br") {
+        msg = "BROMINE:";
+    }
+    else if (element == "Kr") {
+        msg = "KRYPTON:";
+    }
+    else if (element == "Rb") {
+        msg = "RUBIDIUM:";
+    }
+    else if (element == "Sr") {
+        msg = "STRONTIUM:";
+    }
+    else if (element == "Y") {
+        msg = "YTTRIUM:";
+    }
+    else if (element == "Zr") {
+        msg = "SIRCONIUM:";
+    }
+    else if (element == "Nb") {
+        msg = "NIOBIUM:";
+    }
+    else if (element == "Mo") {
+        msg = "MOLYBDENUM:";
+    }
+    else if (element == "Tc") {
+        msg = "TECHNETIUM:";
+    }
+    else if (element == "Ru") {
+        msg = "RUTHENIUM:";
+    }
+    else if (element == "Rh") {
+        msg = "RHODIUM:";
+    }
+    else if (element == "Pd") {
+        msg = "PALLADIUM:";
+    }
+    else if (element == "Ag") {
+        msg = "SILVER:";
+    }
+    else if (element == "Cd") {
+        msg = "CADMIUM:";
+    }
+    else if (element == "In") {
+        msg = "INDIUM:";
+    }
+    else if (element == "Sn") {
+        msg = "TIN:";
+    }
+    else if (element == "Sb") {
+        msg = "ANTIMONY:";
+    }
+    else if (element == "Te") {
+        msg = "TELLURIUM:";
+    }
+    else if (element == "I") {
+        msg = "IODINE:";
+    }
+    else if (element == "Xe") {
+        msg = "XENON:";
+    }
+    else if (element == "Cs") {
+        msg = "CESIUM:";
+    }
+    else if (element == "Ba") {
+        msg = "BARIUM:";
+    }
+    else if (element == "La") {
+        msg = "LANTHANUM:";
+    }
+    else if (element == "Ce") {
+        msg = "CERIUM:";
+    }
+    else if (element == "Pr") {
+        msg = "PRASEODYMIUM:";
+    }
+    else if (element == "Nd") {
+        msg = "NEODYMIUM:";
+    }
+    else if (element == "Pm") {
+        msg = "PROMETHIUM:";
+    }
+    else if (element == "Sm") {
+        msg = "SAMARIUM:";
+    }
+    else if (element == "Eu") {
+        msg = "EUROPIUM:";
+    }
+    else if (element == "Gd") {
+        msg = "GADOLINIUM:";
+    }
+    else if (element == "Tb") {
+        msg = "TERBIUM:";
+    }
+    else if (element == "Dy") {
+        msg = "DYSPROSIUM:";
+    }
+    else if (element == "Ho") {
+        msg = "HOLMIUM:";
+    }
+    else if (element == "Er") {
+        msg = "ERBIUM:";
+    }
+    else if (element == "Tm") {
+        msg = "THULIUM:";
+    }
+    else if (element == "Yb") {
+        msg = "YTTERBIUM:";
+    }
+    else if (element == "Lu") {
+        msg = "LUTETIU0M:";
+    }
+    else if (element == "Hf") {
+        msg = "HAFNIUM:";
+    }
+    else if (element == "Ta") {
+        msg = "TANTALUM:";
+    }
+    else if (element == "W") {
+        msg = "TUNGSTEN:";
+    }
+    else if (element == "Re") {
+        msg = "RHENIUM:";
+    }
+    else if (element == "Os") {
+        msg = "OSMIUM:";
+    }
+    else if (element == "Ir") {
+        msg = "IRIDIUM:";
+    }
+    else if (element == "Pt") {
+        msg = "PLATINUM:";
+    }
+    else if (element == "Au") {
+        msg = "GOLD:";
+    }
+    else if (element == "Hg") {
+        msg = "MERCURY:";
+    }
+    else if (element == "Tl") {
+        msg = "THALLIUM:";
+    }
+    else if (element == "Pb") {
+        msg = "LEAD:";
+    }
+    else if (element == "Bi") {
+        msg = "BISMUTH:";
+    }
+    else if (element == "Po") {
+        msg = "POLONIUM:";
+    }
+    else if (element == "At") {
+        msg = "ASTATINE:";
+    }
+    else if (element == "Rn") {
+        msg = "RADON:";
+    }
+    else if (element == "Fr") {
+        msg = "FRANCIUM:";
+    }
+    else if (element == "Ra") {
+        msg = "RADIUM:";
+    }
+    else if (element == "Ac") {
+        msg = "ACTINIUM:";
+    }
+    else if (element == "Th") {
+        msg = "THORIUM:";
+    }
+    else if (element == "Pa") {
+        msg = "PROTACTINIUM:";
+    }
+    else if (element == "U") {
+        msg = "URANIUM:";
+    }
+    else if (element == "Np") {
+        msg = "NEPTUNIUM:";
+    }
+    else if (element == "Pu") {
+        msg = "PLUTONIUM:";
+    }
+    else if (element == "Am") {
+        msg = "AMERICIUM:";
+    }
+    else if (element == "Cm") {
+        msg = "CURIUM:";
+    }
+    else if (element == "Bk") {
+        msg = "BERKELIUM:";
+    }
+    else if (element == "Cf") {
+        msg = "CALIFORNIUM:";
+    }
+    else if (element == "Es") {
+        msg = "EINSTEINIUM:";
+    }
+    else if (element == "Fm") {
+        msg = "FERMIUM:";
+    }
+    else if (element == "Md") {
+        msg = "MENDELEVIUM:";
+    }
+    else if (element == "No") {
+        msg = "NOBELIUM:";
+    }
+    else if (element == "Lr") {
+        msg = "LAWRENCIUM:";
+    }
+    else if (element == "Rf") {
+        msg = "RUTHERFORDIUM:";
+    }
+    else if (element == "Db") {
+        msg = "DUBNIUM:";
+    }
+    else if (element == "Sg") {
+        msg = "SEABORGIUM:";
+    }
+    else if (element == "Bh") {
+        msg = "BOHRIUM:";
+    }
+    else if (element == "Hs") {
+        msg = "HASSIUM:";
+    }
+    else if (element == "Mt") {
+        msg = "MEITNERIUM:";
+    }
+    else if (element == "Ds") {
+        msg = "DARMSTADTIUM:";
+    }
+    else if (element == "Rg") {
+        msg = "ROENTGENIUM:";
+    }
+    else if (element == "Cn") {
+        msg = "COPERNICIUM:";
+    }
+    else if (element == "Uut") {
+        msg = "UNUNTRIUM:";
+    }
+    else if (element == "Fl") {
+        msg = "FLEROVIUM:";
+    }
+    else if (element == "Uup") {
+        msg = "UNUNPENTIUM:";
+    }
+    else if (element == "Lv") {
+        msg = "LIVERMORIUM:";
+    }
+    else if (element == "Uus") {
+        msg = "UNUNSEPTIUM:";
+    }
+    else if (element == "Uuo") {
+        msg = "UNUNOCTIUM:";
+    }
+document.getElementById('output').innerHTML = msg;
 }
-
