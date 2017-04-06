6289dd29602bc66161c172e9c1b3b157a530eec3
Lucinda A Gebbie <lg73551@srcs.loc>
2017-04-06 13:22:45 -0700

    new commit

--- create.js
+++ create.js
@@ -1,11 +1,18 @@
+function element (name, atomicNum, fact) {
+this.name = name;
+this.atomicNum = atomicNum;
+this.fact = fact;
+}
+var Li = new Element (Lithium, 3, The lightest metal);
+
+
 function elementAlert (element) {
-   var aNum = "Atomic Number:";
-   if (element == "Li") {
-       msg  = "LITHIUM:" + '<br>' + aNum + " 3." + '<br>' + "Lithium is the lightest metal!";
-} else if (element == "Be") {
-    msg = "BROMINE";
- } else if (element ==
+       msg  = name + '<br>' + atomicNum + '<br>' + fact;
+
+
+
 
     document.getElementById("output").innerHTML = msg;
-}
+    //document.getElementById("output").innerHTML = alert alert-info;
 
+}
